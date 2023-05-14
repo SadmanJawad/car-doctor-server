60621339d829e3b6dcc85a965b1f7acf71d4c54f
SadmanJawad <jawadnabil977@gmail.com>
2023-05-14 12:26:34 +0600

    extract booking service data && save order on the order collect. using post method

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services')
+    const bookingCollection = client.db('carDoctor').collection('bookings')
 
     app.get('/services', async(req, res) => {
       const cursor = serviceCollection.find();
@@ -42,13 +43,19 @@ async function run() {
 
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, imdb: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1 },
       };
 
 
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
 
+    })
+
+    // bookings
+    app.post('/bookings', async(req, res) => {
+      const booking = req.body;
+      
     })
 
 
