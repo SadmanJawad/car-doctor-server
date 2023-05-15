5c3ccbec858fff406b2f32986e8428110d17278d
SadmanJawad <jawadnabil977@gmail.com>
2023-05-15 13:21:03 +0600

    diplay bookings and service img in tabular format

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS)
+/* console.log(process.env.DB_PASS) */
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixkqk3t.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -43,7 +43,7 @@ async function run() {
 
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
 
 
@@ -54,7 +54,11 @@ async function run() {
 
     // bookings
     app.get('/bookings', async (req, res) => {
-      console.log(req.query);
+      console.log(req.query.email);
+      let query = {};
+      if(req.query?.email){
+        query={ email: req.query.email }
+      }
       const result = await bookingCollection.find().toArray();
       res.send(result)
     })
